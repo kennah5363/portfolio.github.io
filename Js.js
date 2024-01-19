d2295d67408465d245d7fa164ffc0e5dacca0bf0
kennah5363 <107480303+kennah5363@users.noreply.github.com>
2024-01-19 15:10:31 +0300

    Add files via upload

--- Js.js
+++ Js.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-  strings:["Symo", "Ndirangu", "Edwin"],
+  strings:["Frontend developer", "Hairdresser", "Beautician"],
   typeSpeed:100,
   backSpeed:100,
   backDelay:1000,
